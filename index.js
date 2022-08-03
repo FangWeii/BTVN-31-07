5533ee9deadb152f3f251ff098ca0f463cb1bed6
FangWeii <vodoanphuongvy@gmail.com>
2022-08-03 15:45:05 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('Deep in my bone');
+setTimeout(function() {
+    console.log('I can feel you')
+}, 1000);
+setTimeout(function() {
+    console.log('Take me back')
+}, 2000);
+setTimeout(function() {
+    console.log('To a time only we knew')
+}, 3000);
+setTimeout(function() {
+    console.log('Hide away')
+}, 4000);
